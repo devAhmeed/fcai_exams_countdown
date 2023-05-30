a0c0f61a7be3ce3ec8c00133e657cd1c968fa834
Ahmed Hisham <ahmedhishamkamal@icloud.com>
2023-05-30 20:23:35 +0300

    V1 Init

---
+++ script.js
@@ -0,0 +1,48 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jun 22, 2023 11:00:00").getTime();
+let q = document.querySelector('#demo');
+
+
+
+console.log(q);
+
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+  // Checking if leading 0 digit exists or not 
+
+
+  if(seconds <=9){
+    seconds = "0" + seconds;
+  }
+    if (days <= 9) {
+      days = "0" + days;
+    }
+      if (hours <= 9) {
+        hours = "0" + hours;
+      }
+        if (minutes <= 9) {
+          minutes = "0" + minutes;
+        }
+  // Display the result in the element with id="demo"
+  q.innerHTML =
+    days + " : " + hours + " : " + minutes + " : " + seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
