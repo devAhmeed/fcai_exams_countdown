9b29cc163dd45784419cdbcec80060a1e4c70727
Ahmed Hisham <ahmedhishamkamal@icloud.com>
2024-01-10 19:55:48 +0200

    2nd Version ( UI )

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 22, 2023 11:00:00").getTime();
-let q = document.querySelector('#demo');
+var countDownDate = new Date("Jan 24, 2024 16:30:00").getTime();
+let q = document.querySelector("#demo");
 let m = document.querySelector("#txt");
 let s = document.querySelector(".footer-x");
 let r = document.querySelector(".def");
-
 
 console.log(q);
 
@@ -22,32 +21,29 @@ var x = setInterval(function () {
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
+  // Checking if leading 0 digit exists or not
 
-  // Checking if leading 0 digit exists or not 
-
-
-  if(seconds <=9){
+  if (seconds <= 9) {
     seconds = "0" + seconds;
   }
-    if (days <= 9) {
-      days = "0" + days;
-    }
-      if (hours <= 9) {
-        hours = "0" + hours;
-      }
-        if (minutes <= 9) {
-          minutes = "0" + minutes;
-        }
+  if (days <= 9) {
+    days = "0" + days;
+  }
+  if (hours <= 9) {
+    hours = "0" + hours;
+  }
+  if (minutes <= 9) {
+    minutes = "0" + minutes;
+  }
   // Display the result in the element with id="demo"
-  q.innerHTML =
-    days + " : " + hours + " : " + minutes + " : " + seconds;
+  q.innerHTML = days + " : " + hours + " : " + minutes + " : " + seconds;
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "YOU MADE IT ! <br> Good Luck";
-    m.style.opacity = "0";
-    s.innerHTML = "You're Free Now Solider ❤ , Enjoy ."
-    r.style.opacity = "0";
+    //document.getElementById("demo").innerHTML = "YOU MADE IT ! <br> Good Luck";
+    //m.style.opacity = "0";
+    //s.innerHTML = "You're Free Now Solider ❤ , Enjoy ."
+    //r.style.opacity = "0";
   }
 }, 1000);
