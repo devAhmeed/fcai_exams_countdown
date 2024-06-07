26d42d5661d4e023c928615c8c180090c96c700c
Ahmed Hisham <ahmedhishamkamal@icloud.com>
2024-06-07 15:22:12 +0300

    add : new celebration when timer end

--- script.js
+++ script.js
@@ -33,27 +33,69 @@ var x = setInterval(function () {
   if (minutes <= 9) {
     minutes = "0" + minutes;
   }
-  // Display the result in the element with id="demo"
-  q.innerHTML = days;
-  m.innerHTML = ":" + hours + ":" + minutes + ":" + seconds;
+  randNum = Math.floor(Math.random() * (8 - 1 + 1)) + 1;
+  function changeVideoInc() {
+    if (randNum < 8) {
+      randNum++;
+      document.querySelector("#done-video").src = `videos/v${randNum}.mp4`;
+    } else {
+      randNum = 1;
+      document.querySelector("#done-video").src = `videos/v${randNum}.mp4`;
+    }
+  }
+
+  function changeVideoDec() {
+    if (randNum == 1) {
+      randNum = 8;
+      document.querySelector("#done-video").src = `videos/v${randNum}.mp4`;
+    } else {
+      randNum--;
+      document.querySelector("#done-video").src = `videos/v${randNum}.mp4`;
+    }
+  }
+
+  document.getElementById("next").addEventListener("click", changeVideoInc);
+  document.getElementById("prev").addEventListener("click", changeVideoDec);
+  document.getElementById("pause").addEventListener("click", function () {
+    var video = document.getElementById("done-video");
+    if (video.paused) {
+      video.play();
+      document.getElementById("ps-src").src = "ph_pause-fill.svg";
+    } else {
+      video.pause();
+      document.getElementById("ps-src").src =
+        "https://img.icons8.com/ios-glyphs/30/1A1A1A/play--v1.png";
+    }
+  });
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    // clearInterval(x);
-    // document.querySelector(".def").innerHTML = "6/6 ✔ <br> YOU MADE IT ! <br> 3 Semesters Down , 5 To Go !";
-    // document.querySelector(".ldBar").style.display = "none";
-    // document.querySelector(".on").innerHTML = "Click Anywhere to Sound On";
-    // document.querySelector(".video-done").innerHTML =
-    //   '<video id ="done-video"style="width: 100%; height: 50svh;" autoplay muted loop><source src="./Snaptik.app_7205725798081285381.mp4" type="video/mp4" /></video>';
-    // document
-    //   .addEventListener("click", function () {
-    //     var video = document.getElementById("done-video");
-    //     video.play();
-    //     video.muted = false;
-    //         document.querySelector(".on").style.display = "none";
-    //   });
-    // q.innerHTML ="";
-    // m.innerHTML = "";
-    // s.innerHTML = "You're Free Now Solider ❤ , Enjoy ."
+    clearInterval(x);
+    document.querySelector(".btn-fun").innerHTML =
+      '<img width="30" height="30" src="https://img.icons8.com/ios-filled/50/FFFFFF/confetti.png" class="mx-1" alt="confetti"/> You Made It ! 🥳';
+    document.querySelector(".hours").classList.replace("text-6xl", "text-4xl");
+    document
+      .querySelector(".hh-mm-ss")
+      .classList.replace("text-6xl", "text-4xl");
+    document.querySelector(".btn-congra").style.transform = "translateY(3vh)";
+    document.querySelector(".dd-hh-mm-ss").style.display = "none";
+    document.querySelector(".hours").innerHTML = "Four Semesters Down ... ";
+    document.querySelector(".hh-mm-ss").innerHTML = "Four More To Go !";
+    document.querySelector(".gl").style.display = "none";
+    document.querySelector(
+      ".video-container"
+    ).innerHTML = `<video id="done-video" style="height : 50vh;" autoplay muted loop><source src="videos/v${randNum}.mp4" type="video/mp4" /></video>`;
+    document.addEventListener("click", function () {
+      var video = document.getElementById("done-video");
+      video.muted = false;
+    });
+    document.querySelector(
+      ".note"
+    ).innerHTML = `<span class="font-bold">Two more years to be a </span> <span class="font-bold text-orange-500 orange">Graduate</span> <span class="font-bold">of South Valley National University !</span> </br> Happy <span class="font-bold text-orange-500 orange">Semester End </span> , Hope to see y'all soon !</p>`;
+  
+  } else {
+    // Display the result in the element with id="demo"
+    q.innerHTML = days;
+    m.innerHTML = ":" + hours + ":" + minutes + ":" + seconds;
   }
 }, 1000);
